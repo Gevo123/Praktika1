5162483c180d71cee390bd3ba7f0b4f931e3a865
Gevo123 <61075455+Gevo123@users.noreply.github.com>
2023-10-23 01:24:32 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Lema</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target bg-white" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-lg-4">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+                  <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                    <li class="active"><a href="index.html" class="nav-link">Домой</a></li>
+                    <li><a href="project.html" class="nav-link">Проекты</a></li>
+                    <li ><a href="services.html" class="nav-link">Услуги</a></li>
+                  </ul>
+              </nav>
+            </div>
+            <div class="col-lg-4 text-center">
+              <div class="site-logo">
+                <a href="index.html">Lema</a>
+              </div>
+
+
+              <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-primary"></span></a></div>
+            </div>
+            <div class="col-lg-4">
+              <nav class="site-navigation text-left mr-auto " role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="about.html" class="nav-link">О Нас</a></li>
+                  <li><a href="contact.html" class="nav-link">Контакты</a></li>
+                </ul>
+              </nav>
+            </div>
+            
+
+          </div>
+        </div>
+
+      </header>
+
+
+
+
+    <div class="owl-carousel-wrapper">
+
+      
+
+      <div class="box-92819">
+        <div class="owl-carousel slide-one-item-alt-text">
+
+          <div>
+            <h1 class="text-uppercase mb-3">Мы являемся Специалистами в области архитектуры</h1>
+            <p class="mb-5">Наша команда дизайнеров профисеоналы в своём деле</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Связаться с нами</a></p>
+          </div>
+
+          <div>
+            <h1 class="text-uppercase mb-3">Дизайн, благодаря которому Вы будете чувствовать себя как дома</h1>
+            <p class="mb-5">мы поможем подобрать решение которое будет для вас отпимальнвым</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Связаться с нами</a></p>
+          </div>
+
+          <div>
+            <h1 class="text-uppercase mb-3">Готовы приступить к реализации Вашего Домашнего Проекта</h1>
+            <p class="mb-5">Если у вас есть идеи или наброски, то мы готовы работать по нему</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Связаться с нами</a></p>
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div class="owl-carousel owl-1 ">
+        <div class="ftco-cover-1" style="background-image: url('images/hero_1.jpg');"></div>
+        <div class="ftco-cover-1" style="background-image: url('images/hero_2.jpg');"></div>
+        <div class="ftco-cover-1" style="background-image: url('images/hero_3.jpg');"></div>
+        
+      </div>
+    </div>
+
+    
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-4">
+            <div class="h-100 bg-white box-29291">
+              <h2 class="heading-39291">Добро пожаловать в<br> нашу компанию</h2>
+              <p>Наша компания является официальным представителем и прямым поставщиком мебели многих Российский фабрик.</p>
+              <p>На нашем складе постоянно поддерживается большой ассортимент мебели общим объемом более 300 тонн. Это позволяет нам удовлетворять самые разные заказы на мебель и производить отгрузку товара в день заказа.</p>
+               <p>Процессы, требующие точности, автоматизированы. Наши мастера вкладывают в каждый предмет мебели душевное тепло и долю уникальной неповторимости — в лучших традициях мебельных фабрик Европы и Скандинавии.</p>
+
+              <p class="mt-5">
+              
+                
+               
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="owl-carousel owl-3">
+              <img src="images/about_1.jpg" alt="Image" class="img-fluid">
+              <img src="images/about_2.jpg" alt="Image" class="img-fluid">
+              <img src="images/about_3.jpg" alt="Image" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 align-items-center">
+          <div class="col-md-7">
+            <h2 class="heading-39291 mb-0">Что мы делаем</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/001-stairs.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Креативная лестница</a></h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/002-kitchen.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Дизайн кухни</a></h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/003-lamp.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Украшение лампы</a></h3>
+              <p></p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/004-blueprint.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Проект интерьера</a></h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/005-dinning-table.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Обеденный стол</a></h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/006-pantone.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Современный дизайн</a></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 align-items-center">
+          <div class="col-md-7">
+            <h2 class="heading-39291 mb-0">Наши проекты</h2>
+          </div>
+          <div class="col-md-5 text-right">
+            <p class="mb-0"><a href="#" class="more-39291">Просмотреть все проекты</a></p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link small"><img src="images/img_1.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Нижняя сторона дома</a></h3>
+              <span>Саратов, Россия</span>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link"><img src="images/img_2.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Кухня квартиры</a></h3>
+              <span>Москва, Россия</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link"><img src="images/img_3.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Гостиная</a></h3>
+              <span>Сочи, Россия</span>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link small"><img src="images/img_4.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Кухня дома</a></h3>
+              <span>Омск, Россия</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+     
+
+    
+    
+
+    
+    
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+
+  </body>
+
+</html>
+
